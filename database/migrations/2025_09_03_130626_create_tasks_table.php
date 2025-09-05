25434cd2fec352f42dfdefc423823be09ea13f6e
gornostaev <gornostaev.s19@gmail.com>
2025-09-05 14:50:07 +0300

    init

---
+++ database/migrations/2025_09_03_130626_create_tasks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('title', 255)->nullable(false);
+            $table->string('description', 255);
+            $table->string('status', 255);
+            $table->timestamp('due_date');
+
+            $table
+                ->foreign(['user_id'], 'fk-tasks_users')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
